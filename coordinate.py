f4a86f4c3ec5b0fad44fb5a62d485accd8b293ef
NonmailapzzZ <149261623+NonmailapzzZ@users.noreply.github.com>
2025-10-28 15:49:15 +0700

    corrected inverse kinametic equation

--- coordinate.py
+++ coordinate.py
@@ -30,21 +30,22 @@ def tranformation_matrix(theta:int, index:int) -> numpy.array :
 
     return tranformation
 
-def inverse_matrix(x:float, y:float)  :
+def inverse_matrix(x, y) :
+    x = numpy.float64(x)
+    y = numpy.float64(y)
     # include a link length
-    link1 = dh_parameter[0]['r']
-    link2 = dh_parameter[1]['r']
+    link1 = numpy.float64(dh_parameter[0]['r'])
+    link2 = numpy.float64(dh_parameter[1]['r'])
     if (x**2+y**2) > (link1**2+link2**2) :
         raise ValueError("your coordinate destination is out of range")
     
     
     r = numpy.sqrt((x**2)+(y**2))
-    phi_1 = numpy.acos((link2**2-(link1**2+r**2))/2*link1*r)
-    phi_2 = numpy.atan(y/x)
+    phi_1 = numpy.arccos((link2**2 - link1**2 + r**2)/(2*link1*r))
+    phi_2 = numpy.arctan(y/x)
     theta_1 = phi_2-phi_1
 
-    phi_3 = numpy.acos((r**2-(link1**2+link2**2))/2*link1*link2)
+    phi_3 = numpy.arccos((r**2 - link1**2 + link2**2)/(2*link1*link2))
     theta_2 = (pi/2)-phi_3
     
     return theta_1, theta_2
-
