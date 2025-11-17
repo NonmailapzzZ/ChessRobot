76422297aa8276bda39e6edc25073ec502b57e25
NonmailapzzZ <149261623+NonmailapzzZ@users.noreply.github.com>
2025-11-17 22:58:50 +0700

    0 for 90Degree 1 for 360Degree Servo

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=8)
 # setup 
 kit.servo[0].angle = 0
-kit.servo[1].angle = 0
+kit.continuous_servo[1].throttle = 0
 time.sleep(5)
 # set home
 kit.servo[0].angle = 90
-kit.servo[1].angle = 90
+kit.continuous_servo[1].throttle = 1
 
