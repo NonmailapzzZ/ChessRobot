ef9c071792b165a59f668ccbe734c1fb63bcea79
NonmailapzzZ <149261623+NonmailapzzZ@users.noreply.github.com>
2025-12-14 19:49:17 +0700

    CHANGE SPEED

--- Origin/servo_controller.py
+++ Origin/servo_controller.py
@@ -7,7 +7,7 @@ kit = ServoKit(channels=8)
 
 position = 0
 
-def move_slow_link1(target, step=1, delay=0.02):
+def move_slow_link1(target, step=1, delay=0.07):
     target = 180 - int(target)
     channel = 0
     current = kit.servo[channel].angle
@@ -47,7 +47,7 @@ class move_slow_link2 :
         # update pos
         self.pos = target_deg
     
-def move_slow_slider(target, step=1, delay=0.02):
+def move_slow_slider(target, step=1, delay=0.07):
     target = int(target)
     channel = 4
     current = kit.servo[channel].angle
