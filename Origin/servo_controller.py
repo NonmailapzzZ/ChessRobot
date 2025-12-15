bca09dd34b9f4a9571fc8b403874affeade899fb
NonmailapzzZ <149261623+NonmailapzzZ@users.noreply.github.com>
2025-12-15 19:04:45 +0700

    change servo controll on link1

--- Origin/servo_controller.py
+++ Origin/servo_controller.py
@@ -42,22 +42,21 @@ def duty_to_angle(duty):
 # = = = = = = = = = = = = = = = = = = = = 
 
 def move_slow_link1(target, step=1, delay=0.04):
-
+    target = 180 - target + 89
     SERVO_CH = 0
     current = pca.channels[SERVO_CH].duty_cycle
     current = duty_to_angle(current)
     
     if current < target:
-        for angle in range(int(current), target + 1, step):
-            pca.channels[SERVO_CH].duty_cycle = angle_to_duty(angle)
+        for angle in range(int(current), target + 5, step):
+            pca.channels[SERVO_CH].duty_cycle = angle_to_duty(angle) - 44
             time.sleep(delay)
     else:
-        for angle in range(int(current), target - 1, -step):
-            pca.channels[SERVO_CH].duty_cycle = angle_to_duty(angle)
+        for angle in range(int(current), target - 5, -step):
+            pca.channels[SERVO_CH].duty_cycle = angle_to_duty(angle) - 88
             time.sleep(delay)
 
 class move_slow_link2 :
-    
     def __init__(self, kit=kit, channel=2, deg_per_sec=360/7.39, forward_servo_speed = 0.2, backward_servo_speed = -0.31):
         self.kit = kit
         self.channel = channel
@@ -69,11 +68,9 @@ class move_slow_link2 :
     def move(self, target_deg):
         delta = int(target_deg) - self.pos
         duration = abs(delta) / self.deg_per_sec
-
         self.kit.continuous_servo[self.channel].throttle = self.backward_speed if delta < 0 else self.forward_speed
         time.sleep(duration)
         self.kit.continuous_servo[self.channel].throttle = 0
-        
         # update pos
         self.pos = target_deg
     
@@ -81,12 +78,6 @@ def move_slow_slider(target, step=1, delay=0.04):
     target = int(target)
     channel = 4
     current = kit.servo[channel].angle
-
-    if current is None:
-        current = 70
-        kit.servo[channel].angle = current
-        time.sleep(.5)
-
     if current < target:
         for angle in range(int(current), target + 1, step):
             kit.servo[channel].angle = angle
@@ -99,14 +90,3 @@ def move_slow_slider(target, step=1, delay=0.04):
 def calibration_servo() :
     kit.servo["link1"].angle = 180
     kit.servo["slider"].angle = 70
-
-
-# for grippper maximum for grab
-# move_slow(0, 0)
-
-# for gripprt maximum for lifting gripper
-# move_slow(0, 70)
-
-
-# test
-# move_slow_link1(180)
